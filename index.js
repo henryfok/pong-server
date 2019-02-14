68340387f4769d43106fd7a2d2196605b30cfb1d
Henry <himleungfok@hotmail.com>
2019-02-14 14:25:23 -0800

    Missing semicolon...

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-const port=process.env.PORT || 3000
+const port=process.env.PORT || 3000;
 
 http.listen(port, function() {
 	console.log('listening on *:' + port);
